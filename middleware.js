99d3db174594fbc69fe32bad77cae050127c442d
urvashi-lab <urvashiipatil05@gmail.com>
2025-10-24 00:26:02 +0530

    Fixed bad Getaway

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
         req.flash("error","You need to login first!");
-        res.redirect("/login")
+        return res.redirect("/login");
       }
       next();
 }
